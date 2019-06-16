2b5a142904d1ee92ee5a865c130c43951ef44ff2
rktel <rakinakuy@hotmail.com>
2019-06-16 18:38:02 -0500

    add vue leaflet 2

--- client/index.js
+++ client/index.js
@@ -1,9 +1,31 @@
 import Vue from 'vue';
+
+// VueMeteorTracker
 import VueMeteorTracker from 'vue-meteor-tracker';   // here!
 Vue.use(VueMeteorTracker);                           // here!
+// Vuetify CSS
+import Vuetify from 'vuetify'
+Vue.use(Vuetify)
+import 'vuetify/dist/vuetify.min.css'
 
+// add App.vue
 import App from '../imports/ui/App.vue';
+// Quitamos mensaje Vue Development de consola del navegador
+Vue.config.productionTip = false
 
+// Vue-Leaflet
+import { Icon }  from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+
+
+// this part resolve an issue where the markers would not appear
+delete Icon.Default.prototype._getIconUrl;
+
+Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
 
 Meteor.startup(() => {
   new Vue({
