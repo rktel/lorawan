76c39c059ab33e1b3f42fd3dbf335238876a5001
rktel <rakinakuy@hotmail.com>
2019-06-16 15:02:17 -0500

    change data json

--- server/core/methods.js
+++ server/core/methods.js
@@ -2,6 +2,14 @@ import { Data } from '../../imports/collections'
 
 Meteor.methods({
     insertData(data) {
-        Data.insert({ data })
+        const dataArray = data.split(",");
+        const deviceID = dataArray[0];
+        const latitud = dataArray[1];
+        const longitud = dataArray[2];
+        const datetime = dataArray[3];
+        data = {
+            deviceID, latitud,longitud,datetime
+        }
+        Data.insert( data )
     }
 });
