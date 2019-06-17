53258da22b731e869a8b1f749357472af96553c6
rktel <rakinakuy@hotmail.com>
2019-06-16 23:13:03 -0500

    add vue leaflet 8

--- imports/ui/App.vue
+++ imports/ui/App.vue
@@ -1,17 +1,49 @@
 <template>
-    <LMap></LMap>
+  <div style="height: 100%; width: 100%">
+    <div class="info" style="height: 15%">
+      <span>Center: {{ center }}</span>
+      <span>Zoom: {{ zoom }}</span>
+      <span>Bounds: {{ bounds }}</span>
+    </div>
+    <LMap
+      style="height: 80%; width: 100%"
+      :zoom="zoom"
+      :center="center"
+      @update:zoom="zoomUpdated"
+      @update:center="centerUpdated"
+      @update:bounds="boundsUpdated"
+    >
+      <LTileLayer :url="url"></LTileLayer>
+    </LMap>
+  </div>
 </template>
 
 <script>
-import {LMap, LTileLayer, LMarker } from 'vue2-leaflet';
+
+
+import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
+
 
 export default {
-    components:{
-        LMap, LTileLayer, LMarker
+    components:{ LMap, LTileLayer, LMarker },
+  data () {
+    return {
+      url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+      zoom: 3,
+      center: [47.413220, -1.219482],
+      bounds: null
+    };
+  },
+  methods: {
+    zoomUpdated (zoom) {
+      this.zoom = zoom;
+    },
+    centerUpdated (center) {
+      this.center = center;
+    },
+    boundsUpdated (bounds) {
+      this.bounds = bounds;
     }
+  }
 }
 </script>
-
-<style>
-
-</style>
