af5481f543dc6a1f74555e6fbe0c02b2f039de1f
rktel <rakinakuy@hotmail.com>
2019-06-16 21:45:19 -0500

    add vue leaflet 4

--- client/index.js
+++ client/index.js
@@ -19,22 +19,15 @@ import 'leaflet/dist/leaflet.css'
 
 
 // this part resolve an issue where the markers would not appear
-/*delete Icon.Default.prototype._getIconUrl;
+delete Icon.Default.prototype._getIconUrl;
 
 Icon.Default.mergeOptions({
-  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-  iconUrl: require('leaflet/dist/images/marker-icon.png'),
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+  iconRetinaUrl: import('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: import('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: import('leaflet/dist/images/marker-shadow.png')
 });
-*/
-import L from 'leaflet';
 
-L.Icon.Default.imagePath = '/';
-L.Icon.Default.mergeOptions({
-    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-    iconUrl: require('leaflet/dist/images/marker-icon.png'),
-    shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
-});
+import L from 'leaflet';
 
 Meteor.startup(() => {
   new Vue({
